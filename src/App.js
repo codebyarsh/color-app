d2a742a4ec9833174d56603802cf4eae1f1e0b48
Arshad Ansari <codebyarsh@gmail.com>
2022-09-05 13:04:24 +0530

    Style ColorBox

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Component } from "react";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
-import "./App.css";
+// import "./App.css";
 
 class App extends Component {
   render() {
