ad8b9530b9aa919d8623032973e0a490443198fa
codebyarsh <codebyarsh@gmail.com>
2022-09-06 18:40:41 +0530

    added class in App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { Component } from "react";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
+import { generatePalette } from "./colorHelpers";
 // import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Palette {...seedColors[6]} />
+      <div className="App dice">
+        <Palette palette={generatePalette(seedColors[4])} />
       </div>
     );
   }
